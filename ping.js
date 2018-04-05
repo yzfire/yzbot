a6871f4bc1219b11484f16fd106dbab1a9d20357
yzfire <37228016+yzfire@users.noreply.github.com>
2018-04-05 21:43:36 +0100

    Delete ping.js

--- ping.js
+++
@@ -1,5 +0,0 @@
-module.exports.run = (client, msg, args) => {
-  const m = msg.channel.send('Pong!...').then(message => {
-      message.edit(`:ping_pong: Pong! The ping is currently: **${message.createdTimestamp-msg.createdTimestamp}ms**`);
-  });
-}
